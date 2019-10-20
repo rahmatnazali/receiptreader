2c25abc58a9f2c7d84960d8023f36ec22eefd0e3
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-10-20 10:31:02 +0700

    post admin add hooks function

--- receipts/receiptreader/admin.py
+++ receipts/receiptreader/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from receiptreader import models
+import receiptreader.helper
 
-# Register your models here.
+def process_all_images(obj):
+    # todo: process all the sub images here
+    for image in obj.image_set.all():
+        if not image.raw_ocr_result:
+            raw_json = receiptreader.helper.textify_binary(image.absolute_path())
+            image.raw_ocr_result = raw_json
+            image.save()
 
-
-# Custom Admin
 
 # For raw receipt
 class ImageInline(admin.TabularInline):
@@ -12,24 +17,30 @@ class ImageInline(admin.TabularInline):
     fields = ('binary',)
     min_num = 1
 
+    def get_fields(self, request, obj=None):
+        if obj is None:
+            return 'binary',
+        else:
+            return 'binary', 'raw_ocr_result'
+
 class RawReceiptAdmin(admin.ModelAdmin):
     inlines = [
         ImageInline
     ]
 
     def response_add(self, request, obj, post_url_continue=None):
-        print('response_add')
-        print(obj)
-        print(obj.image_set.all())
-
-        # todo: process all the sub images here
-
+        process_all_images(obj)
         return super().response_add(request, obj, post_url_continue)
 
+    def response_change(self, request, obj):
+        process_all_images(obj)
+        return super().response_change(request, obj)
 
 admin.site.register(models.RawReceipt, RawReceiptAdmin)
-admin.site.register(models.Image)
-# admin.site.register(models.RawJson)
+
+
+## Debug purpose
+# admin.site.register(models.Image)
 
 
 # For processed receipt
--- receipts/receiptreader/models.py
+++ receipts/receiptreader/models.py
@@ -19,26 +19,12 @@ class Image(models.Model):
     raw_ocr_result = models.TextField(verbose_name='Raw OCR Result (let it empty)', null=True, blank=True)
     timestamp = models.DateField(auto_now=False, auto_now_add=True)
 
+    def absolute_path(self):
+        return str(pathlib.Path(self.binary.name).absolute())
+
     def __str__(self):
         # return str(self.binary.name)
         return str(pathlib.Path(self.binary.name).absolute())
-
-def raw_receipt_post_save(sender, instance = None, **kwargs):
-    print("Instance raw receipt = ", instance)
-    print(type(instance))
-    print(instance.image_set.all())
-
-
-def image_post_save(sender, image_instance = None, **kwargs):
-    print("Instance image = ", image_instance)
-    print(type(image_instance))
-    # if image_instance:
-    #     image_instance.raw_ocr_result = textify_binary(image_instance.__str__())
-    #     image_instance.save()
-
-post_save.connect(image_post_save, sender=Image)
-post_save.connect(raw_receipt_post_save, sender=RawReceipt)
-
 
 # #Output after document is read by Google vision and JSON is returned
 
