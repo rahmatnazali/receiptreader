345af0dbbe0cc50d6213c60318c6285548ff0e66
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-11-09 07:45:38 +0700

    add drf and drf-jwt

--- receipts/receipts/settings.py
+++ receipts/receipts/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework_jwt'
+
     'general',
     'receiptreader',
 
