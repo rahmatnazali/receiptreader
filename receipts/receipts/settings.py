1ca90a6ca03514afbf92c67553f65cb892372ac5
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-11-09 07:54:50 +0700

    add application and url

--- receipts/receipts/settings.py
+++ receipts/receipts/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_framework_jwt'
+    'rest_framework_jwt',
 
     'general',
     'receiptreader',
--- receipts/receipts/urls.py
+++ receipts/receipts/urls.py
@@ -21,6 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('receiptreader/', include('receiptreader.urls')),
     path('admin/', admin.site.urls),
-
-
+    path('api/', include('api.urls'))
 ]
