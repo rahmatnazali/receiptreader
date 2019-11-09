b4f12b5fb305d71a2de2de005f9984bfc491976b
rahmatnazali95 <rahmatnazali95@gmail.com>
2019-11-09 20:37:30 +0700

    update url

--- receipts/api/urls.py
+++ receipts/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import LoginView, RawReceiptListCreateView, RawReceiptDetailView
+from .views import LoginView, RawReceiptListCreateView, RawReceiptDetailView, ProcessedReceiptListCreateView, ProcessedReceiptDetailView
 
 urlpatterns = [
     path('auth/login/', LoginView.as_view(), name='auth-login'),
-    path('receipt/rawreceipt/', RawReceiptListCreateView.as_view(), name='receipt-list'),
-    path('receipt/rawreceipt/<int:pk>', RawReceiptDetailView.as_view(), name='receipt-details'),
+    path('receipt/raw/', RawReceiptListCreateView.as_view(), name='raw-receipt-list'),
+    path('receipt/raw/<int:pk>', RawReceiptDetailView.as_view(), name='raw-receipt-details'),
+    path('receipt/processed/', ProcessedReceiptListCreateView.as_view(), name='processed-receipt-list'),
+    path('receipt/processed/<int:pk>', ProcessedReceiptDetailView.as_view(), name='processed-receipt-details'),
 ]
